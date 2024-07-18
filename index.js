41e2dee1746e63dcc093c9a50093b66962f4f657
Zuonx <giljueon@gmail.com>
2024-07-18 23:29:20 +0900

    commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 
+// 'public' 디렉토리를 정적 파일 제공 디렉토리로 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(8080, function () {
     console.log('server start on 8080');
 });
@@ -10,7 +13,3 @@ app.listen(8080, function () {
 app.get('/', function (req, res) { 
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-
-
-
